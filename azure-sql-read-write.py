5a20d09fcbd6cded61c91cfdf132a4ba1c70cd0f
rya00066@gmail.com <rya00066@gmail.com>
2022-09-20 20:33:54 +0000

    scala updates

--- azure-sql-read-write.py
+++ azure-sql-read-write.py
@@ -11,8 +11,18 @@
 
 # COMMAND ----------
 
+# MAGIC %scala
+# MAGIC import com.microsoft.sqlserver.jdbc.spark
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC LIST JAR;
+
+# COMMAND ----------
+
 import pyspark
-import pyspark.pandas as ps
+# import pyspark.pandas as ps
 # Create Dataframe
 df = spark.sql("SELECT * FROM default.nyc_taxi")
 
@@ -41,6 +51,77 @@ df2 = ps.sql("SELECT * FROM default.nyc_taxi")
 
 # MAGIC %md
 # MAGIC #Write Data to Azure SQL Database
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Convert a Spark DataFrame to Scala DataFrame
+
+# COMMAND ----------
+
+# create a temp view with python spark dataframe
+df.createOrReplaceTempView("TempView")
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC val scala_df = table("TempView")
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC import org.apache.spark.sql.types.StructType
+# MAGIC import org.apache.spark.sql.functions._
+# MAGIC import org.apache.spark.sql.types.StructType
+# MAGIC import org.apache.spark.sql.types._
+# MAGIC import com.microsoft.sqlserver.jdbc.spark
+# MAGIC 
+# MAGIC val server_name = "jdbc:sqlserver://testsqlserver03.database.windows.net"
+# MAGIC val database_name = "testdb01"
+# MAGIC val url = "jdbc:sqlserver://testsqlserver03.database.windows.net:1433;database=testdb01"
+# MAGIC val user = "notTheAdmin"
+# MAGIC val password = "notThePassword$10"
+# MAGIC 
+# MAGIC 
+# MAGIC val tableName1 = "nyc_taxi"
+# MAGIC 
+# MAGIC scala_df.write
+# MAGIC .format("com.microsoft.sqlserver.jdbc.spark")
+# MAGIC .mode("append")
+# MAGIC .option("truncate" ,true)
+# MAGIC .option("url", url)
+# MAGIC .option("dbtable", tableName1)
+# MAGIC .option("user", user)
+# MAGIC .option("password", password)
+# MAGIC .option("reliabilityLevel", "BEST_EFFORT")
+# MAGIC .option("tableLock", "false")
+# MAGIC .option("batchsize", "1048576")
+# MAGIC .save()
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC import com.microsoft.azure.sqldb.spark.bulkcopy.BulkCopyMetadata
+# MAGIC import com.microsoft.azure.sqldb.spark.config.Config
+# MAGIC import com.microsoft.azure.sqldb.spark.connect._
+# MAGIC 
+# MAGIC /*
+# MAGIC val bulkCopyConfig = Config(Map(
+# MAGIC   "url"               -> "jdbc:sqlserver://testsqlserver03.database.windows.net:1433;database=testdb01",
+# MAGIC   "databaseName"      -> "testdb01",
+# MAGIC   "user"              -> "notTheAdmin",
+# MAGIC   "password"          -> "notThePassword$10",
+# MAGIC   "dbTable"           -> "dbo.Clients",
+# MAGIC   "bulkCopyBatchSize" -> "2500",
+# MAGIC   "bulkCopyTableLock" -> "true",
+# MAGIC   "bulkCopyTimeout"   -> "600"
+# MAGIC ))
+# MAGIC 
+# MAGIC 
+# MAGIC scala_df.bulkCopyToSqlDB(bulkCopyConfig, bulkCopyMetadata)
+# MAGIC 
+# MAGIC scala_df.bulkCopyToSqlDB(bulkCopyConfig)
+# MAGIC */
 
 # COMMAND ----------
 
@@ -170,7 +251,7 @@ df_pandas = df.toPandas()
 
 # COMMAND ----------
 
-df_pandas_duplicated = pd.concat([df_pandas]*1854 , ignore_index=True)
+df_pandas_duplicated = pd.concat([df_pandas]*1854*3 , ignore_index=True)
 
 # COMMAND ----------
 
@@ -350,6 +431,122 @@ help(df_large.sort)
 .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") 
 .save()
 )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Write large data with Scala
+
+# COMMAND ----------
+
+df_large.count()
+
+# COMMAND ----------
+
+
+df_large['passengerCount'].astype('INT')
+
+# COMMAND ----------
+
+pan_df = df_large.toPandas()
+
+# COMMAND ----------
+
+pan_df['passengerCount'].astype(int)
+
+# COMMAND ----------
+
+from pyspark.sql.types import *
+
+# COMMAND ----------
+
+dir(explore_t)
+
+# COMMAND ----------
+
+help(df_large.schema)
+
+# COMMAND ----------
+
+help(pyspark.sql.types.StructType)
+
+# COMMAND ----------
+
+import pyspark.sql.types
+
+mySchema = StructType([StructField("passengerCount", IntegerType(), True),
+            StructField("tripDistance", DoubleType(), True),
+            StructField("hour_of_day", IntegerType(), True),
+            StructField("month_num", IntegerType(), True),
+            StructField("normalizeHolidayName", StringType(), True),
+            StructField("isPaidTimeOff", BooleanType(), True),
+            StructField('isPaidTimeOff', BooleanType(), True),
+            StructField('snowDepth',  DoubleType(), True),
+            StructField('precipTime',  DoubleType(), True),
+            StructField('precipDepth', DoubleType(), True),
+            StructField('temperature',  DoubleType(), True),
+            StructField('totalAmount',  DoubleType(), True)
+           ])
+
+# COMMAND ----------
+
+df_large._schema = mySchema
+
+# COMMAND ----------
+
+df_large
+
+# COMMAND ----------
+
+# write to table
+print(f'Size of Table written to temp table {df_large.count()}')
+df_large.createOrReplaceTempView("scalaWriteTable")
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC val scala_df = table("scalaWriteTable")
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC scala_df.withColumn("passengerCount", $"passengerCount".cast("int"))
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC scala_df
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC import org.apache.spark.sql.types.StructType
+# MAGIC import org.apache.spark.sql.functions._
+# MAGIC import org.apache.spark.sql.types.StructType
+# MAGIC import org.apache.spark.sql.types._
+# MAGIC import com.microsoft.sqlserver.jdbc.spark
+# MAGIC 
+# MAGIC val server_name = "jdbc:sqlserver://testsqlserver03.database.windows.net"
+# MAGIC val database_name = "testdb01"
+# MAGIC val url = "jdbc:sqlserver://testsqlserver03.database.windows.net:1433;database=testdb01"
+# MAGIC val user = "notTheAdmin"
+# MAGIC val password = "notThePassword$10"
+# MAGIC 
+# MAGIC 
+# MAGIC val tableName1 = "nyc_taxi_4"
+# MAGIC 
+# MAGIC scala_df.write
+# MAGIC .format("com.microsoft.sqlserver.jdbc.spark")
+# MAGIC .mode("overwrite")
+# MAGIC .option("truncate" ,true)
+# MAGIC .option("url", url)
+# MAGIC .option("dbtable", tableName1)
+# MAGIC .option("user", user)
+# MAGIC .option("password", password)
+# MAGIC .option("reliabilityLevel", "BEST_EFFORT")
+# MAGIC .option("tableLock", "true")
+# MAGIC .option("batchsize", "1048576")
+# MAGIC .save()
 
 # COMMAND ----------
 
